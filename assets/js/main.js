df54be7c98fb6adb37829512de240981e90874e2
Marcelo <mrcelo.antunes@gmail.com>
2017-12-02 17:07:17 -0200

    Include ajax calls to web chat API - Add necessary calls to the rest API - Add basic logics to login/register

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+var input_user = "";
+var connected_user = "";
+
+$("#form-login-body, #form-login-nav, #form-register-body").submit(function(e) {
+    e.preventDefault();
+});
+
+// Validate user input from navbar as well as from the main form in the center of screen
+function get_user_input(){
+  input_user = $('#input-user').val();
+  if(input_user == "")
+    input_user = $('#input-user-form').val();
+}
+
+function get_user_messages(nextActionType){
+  var jqXHR = $.get("http://www.angelito.com.br/webchat/messages?nickname=" + input_user, function (data) {
+      start_next_action(nextActionType, data);
+  })
+    .fail(function (data) {
+       show_request_error(data);
+    });
+}
+
+function start_next_action(nextActionType, data){
+  if(nextActionType == "login"){
+    // load page
+    // sucess message
+    if(data.length > 21){
+
+    }
+  }
+  else if(nextActionType == "register"){
+    if(data.length > 21){
+      alert('usuário já existe');
+    }
+    else{
+      $.post("http://www.angelito.com.br/webchat/user?nickname=" + input_user, function (data) {
+          load_chat_page(data);
+      })
+       .fail(function (data) {
+         show_request_error(data);
+       });
+    }
+  }
+}
+
+function load_chat_page(data){
+  // Load chat page
+}
+
+function show_request_error(data) {
+  // Show error
+  alert('erro');
+}
+
+function login() {
+  get_user_input();
+  get_user_messages("login");
+}
+
+function register() {
+  get_user_input();
+  get_user_messages("register");
+}
