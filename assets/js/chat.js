1b6d9fc18a3e0f4458743dab401190fef9a0dcf8
Marcelo <mrcelo.antunes@gmail.com>
2017-12-04 20:22:35 -0200

    Separate HTML files for messages - Include separate HTML, CSS and JS for messages page - Add JS rotine to recursively add messages to the page

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,4 +1,5 @@
-
+var number_of_connected_users = 0;
+var number_of_messages = 0;
 
 function load_send_message_painel() {
   get_user_initial();
@@ -7,6 +8,8 @@ function load_send_message_painel() {
   // Inject chat.html
    $("#main").load("/assets/includes/chat.html", function() {
       $("#user-initial").attr("data-letters", connected_user_initial.toUpperCase());
+      // Load all load all messages
+      load_messages_from_server();
    });
    // Set user name nav display
    $("#connected_user").text(connected_user);
@@ -18,7 +21,58 @@ function load_send_message_painel() {
    $("#input-button").attr("onclick", "logoff()");
 }
 
-function get_user_initial() {
-  connected_user = input_user;
-  connected_user_initial = connected_user[0] + connected_user[connected_user.length - 1];
+function load_messages_from_server() {
+  $.getJSON("http://www.angelito.com.br/webchat/messages?nickname=" + input_user, function (data) {
+      // Clean previous html
+      $("#all-messages-container").empty();
+      load_single_message(null, data, 0);
+  })
+    .fail(function (data) {
+       show_request_error(data);
+    });
+}
+
+function load_single_message(html_text, all_messages, current_index) {
+    localStorage.setItem("current_index", current_index.toString());
+
+    if(current_index == 0){
+      $.ajax({ type: "GET",
+         url: "/assets/includes/message.html",
+         success : function(text)
+         {
+             current_index = localStorage.getItem("current_index");
+
+             append_message_into_page(text, all_messages, current_index);
+         }
+      });
+    }
+    else if(html_text != null){
+      append_message_into_page(html_text, all_messages, current_index);
+    }
+}
+
+function append_message_into_page(html_text, all_messages, current_index){
+  var current_message_user = all_messages[current_index].user;
+  var current_message_datetime = all_messages[current_index].datetime;
+  var current_message_text = all_messages[current_index].textmsg;
+
+  $('#all-messages-container').append(html_text);
+
+  $(".message-user-initial").last().attr("data-letters", get_user_initial(current_message_user).toUpperCase());
+  $(".message-user-name").last().text(current_message_user);
+  $(".message-time").last().text(current_message_datetime);
+  $(".message-text").last().html(current_message_text);
+
+  if(Number(current_index) < all_messages.length - 1)
+    load_single_message(html_text, all_messages, Number(current_index) + 1);
+}
+
+function get_user_initial(user_params) {
+  if(user_params == undefined){
+    connected_user = input_user;
+    connected_user_initial = connected_user[0] + connected_user[connected_user.length - 1];
+  }
+  else {
+    return user_params[0] + user_params[user_params.length - 1];
+  }
 }
