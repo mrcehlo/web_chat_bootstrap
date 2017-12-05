cb20d724a624d8dd7fbe77e4539cdc28fad25014
Marcelo <mrcelo.antunes@gmail.com>
2017-12-05 21:20:47 -0200

    Change chat layout and fix login bug - Alter send message form layout - Include mock design for connected users display - Fix routine to get user name

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,13 @@ function get_user_input(){
   input_user = $('#input-user').val();
   if(input_user == "")
     input_user = $('#input-user-form').val();
+  if(input_user == "")
+    input_user = $('#input-new-user-form').val();
 }
 
 function get_user_messages(nextActionType){
+  get_user_input();
+
   var jqXHR = $.get("http://www.angelito.com.br/webchat/messages?nickname=" + input_user, function (data) {
       start_next_action(nextActionType, data);
   })
@@ -27,6 +31,8 @@ function get_user_messages(nextActionType){
 }
 
 function start_next_action(nextActionType, data){
+  get_user_input();
+
   if(nextActionType == "login"){
     // load page
     // sucess message
@@ -60,11 +66,9 @@ function show_request_error(data) {
 }
 
 function login() {
-  get_user_input();
   get_user_messages("login");
 }
 
 function register() {
-  get_user_input();
   get_user_messages("register");
 }
